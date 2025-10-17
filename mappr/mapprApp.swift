d1db3902ae7adfc7ad7c32204d7df11d8e6998f0
hashimxkhan <khan521@purdue.edu>
2025-10-17 14:41:49 -0400

    init

---
+++ mappr/mapprApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapprApp.swift
+//  mappr
+//
+//  Created by Hashim Khan on 10/17/25.
+//
+
+import SwiftUI
+
+@main
+struct mapprApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
